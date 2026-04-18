71f4a8eee3c71d0c89f56d2bb21aab07a8882359
Sayed Hisham <55137645+hisham2k9@users.noreply.github.com>
2026-04-18 10:46:17 +0300

    updated flask app wsgi for pa

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+# Path to your project directory
+path = '/home/hisham2k9/qr_root/qr_app'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+
 from flaskApp import app
 # Expose the Flask app as "application" (WSGI standard)
 application = app
